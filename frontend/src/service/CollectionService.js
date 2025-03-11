76d8d73f0cc0f95cd9902521730aecc484deedd8
Prime1Code <maxi.jaku@gmail.com>
2025-03-11 23:21:59 +0100

    fix url typo

--- frontend/src/service/CollectionService.js
+++ frontend/src/service/CollectionService.js
@@ -35,7 +35,7 @@ export async function showCollectionContent (action, query, page) {
               database +
               "/collections/" +
               collection +
-              "/find/?query=" +
+              "/find?query=" +
               queryText +
               explain +
               limitText +
